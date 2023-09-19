c5d015ec74192b81500991366e6420357b9ce3b9
Isac021 <iksanosmanov57@gmail.com>
2023-09-19 21:43:40 +0300

    a

---
+++ Syntax Exercise/coffee/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace coffee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double finalprice = 0;
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                double pricepercapsule = double.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());
+                int capsules = int.Parse(Console.ReadLine());
+
+
+                double price = ((days * capsules) * pricepercapsule);
+                Console.WriteLine($"The price for the coffee is: ${price:f2}");
+
+                finalprice += price;
+
+
+
+
+
+            }
+            Console.WriteLine($"Total: ${finalprice:f2}");
+        }
+    }
+}
---
+++ Syntax Exercise/rage/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters;
+
+namespace rage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostgames = int.Parse(Console.ReadLine());
+            double headsetprice = double.Parse(Console.ReadLine());
+            int trashedheadset = 0;
+            double mouseprice = double.Parse(Console.ReadLine());
+            int trashedmouse = 0;
+            double keyboardprice = double.Parse(Console.ReadLine());
+            int trashedkeyboard = 0;
+            double displayprice = double.Parse(Console.ReadLine());
+            int displaycount = 0;
+            int trasheddisplay = 0;
+
+            double expenses = 0;
+
+
+            for (int i = 1; i <= lostgames; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    trashedheadset++;
+                }
+                if (i % 3 == 0)
+                {
+                    trashedmouse++;
+                }
+                if (i % 2 == 0 && i % 3 == 0)
+                {
+                    trashedkeyboard++;
+                    if (trashedkeyboard % 2 == 0)
+                    {
+                        displaycount = 1;
+                    }
+                }
+                if (trashedkeyboard % 2 == 0 && displaycount == 1)
+                {
+                    trasheddisplay++;
+                    displaycount = 0;
+
+                }
+            }
+
+            expenses = (trashedheadset * headsetprice) + (trashedmouse * mouseprice) + (trashedkeyboard * keyboardprice) + (trasheddisplay * displayprice);
+            Console.WriteLine($"Rage expenses: {expenses:f2} lv.");
+        }
+    }
+}
